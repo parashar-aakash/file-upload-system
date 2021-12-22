3a460acab370b645a5c7324aa94a0fa3f00a572d
parashar-aakash <aakash.parashar@successive.tech>
2021-12-23 00:54:47 +0530

    Feat: Accept Csv files and convert csv file to array of obj

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import "./App.css";
 import { useState } from "react";
-import FormData from "form-data";
+import { parse } from 'papaparse';
 
 function App() {
   const [ file, setFile ] = useState([]);
   const [ date, setDate ] = useState('');
   
   const fileUpload = ( event ) => {
-    // setFile( event.target.files[0].name);
+    
+    Array.from(event.target.files)
+    .filter((file) => file.type === "text/csv")
+    .forEach((file) => { console.log('file', file)});
+    
     setFile( event.target.files); 
     setDate( event.target.files[0].lastModifiedDate )
+    
+    const arrayOfFilesUploaded = Object.values(event.target.files);
+    arrayOfFilesUploaded.filter( ( file ) => file.type === 'text/csv')
+    .forEach(async (file) => {
+
+      // converted the csv file into object containing data -> array of objects containing data row wise.
+      // using this validation of 5lack row can be applied from here
+      const text = await file.text();
+      const result = parse(text, { header: true });
+      console.log('event', result);
+    })
+
   }
   
-    console.log('event', file, date);
-  
+   
    onsubmit = ( ) => {
-     
+
    }
 
     return (
